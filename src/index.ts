4b1c68f60995d1ef4d42bf73c2bb613164387c70
just-noname-bing <danikfedorov41@gmail.com>
2022-01-29 01:03:48 +0200

    save

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello world");
+import * as express from "express";
+import * as path from "path";
+import * as bodyParser from "body-parser";
+const app = express();
+const PORT = 3002;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.use("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "build/index.html"));
+});
+
+app.listen(PORT, () => console.log(`Running on PORT http://localhost:` + PORT));
